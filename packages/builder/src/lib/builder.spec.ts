c0970fbbc854999aec07bb5684a013133e406793
Mathys-Gasnier <mathys35.gasnier@gmail.com>
2023-11-03 18:54:15 +0100

    Updating tests

--- packages/builder/src/lib/builder.spec.ts
+++ packages/builder/src/lib/builder.spec.ts
@@ -1,9 +1,12 @@
+import { load } from 'loader';
 import { build } from './builder';
 
 describe('builder', () => {
   it('should work', () => {
-    // build("C:\\Users\\matbo\\Documents\\le-static\\test\\simple");
+    const project = load("C:\\Users\\matbo\\Documents\\le-static\\test\\simple");
+    expect(project).toBeTruthy();
+    if(!project) return;
+    build(project);
     // TODO: Checks that the generated dist file is right
-    expect("builder").toEqual('builder');
   });
 });
