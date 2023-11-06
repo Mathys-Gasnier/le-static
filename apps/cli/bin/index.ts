6365772245613f43c83011924ce87e49a4066cad
Mathys-Gasnier <mathys35.gasnier@gmail.com>
2023-11-06 23:12:54 +0100

    Removed commander, created a minimal option parser

--- apps/cli/bin/index.ts
+++ apps/cli/bin/index.ts
@@ -1,13 +1,13 @@
 #!/usr/bin/env node
 
-import { program } from 'commander';
 import { isAbsolute, resolve } from 'path';
 import { build } from '@le-static/builder';
 import { startServer } from '@le-static/server';
 import { Project, load } from '@le-static/loader';
 
+// Builds a project at the passed path or at the current working directory
 function buildProject(project_path?: string): Project {
-      
+  
   let path = project_path ?? process.cwd();
   if(!isAbsolute(path)) path = resolve(process.cwd(), path);
 
@@ -25,20 +25,54 @@ function buildProject(project_path?: string): Project {
   return project;
 }
 
-program
-  .name('les')
-  .option('-p, --project <path>', 'Specify the directory of the project to build')
-  .option('-s, --serve <PORT>', 'Serve the built project on a given port')
-  .action((options) => {
-      const project = buildProject(options.project);
 
-      if(project && options.serve) {
-        console.log(`Starting Server...`);
+// Define the options of the cli
+const options = {
+  project: {
+    flags: [ '-p', '--project' ],
+    value: true
+  },
+  serve: {
+    flags: [ '-s', '--serve' ],
+    value: true
+  }
+};
 
-        startServer(`${project.src}/dist`, {
-          port: options.serve
-        }, project);
-      }
-  });
+// Assumes runned via node, removes node and file name
+const userArgs = process.argv.splice(2);
 
-program.parse();
+// Found options
+const userOptions: Partial<Record<keyof typeof options, true | string>> = { };
+
+// Current arg we are looping on
+let idx = 0;
+while(idx < userArgs.length) {
+  // Get the arg and find if it matches an option
+  const arg = userArgs[idx];
+  const option = Object.entries(options).find(([ , value ]) => value.flags.includes(arg));
+
+  // If not go to the next arg
+  if(!option) {
+    idx++;
+    continue;
+  }
+
+  // If we found an option, get it's value if it's needed, add it the the found options and continue parsing options
+  const [ name, config ] = option;
+
+  const value = config.value ? userArgs[idx + 1] : true;
+
+  userOptions[name] = value;
+  idx += config.value ? 2 : 1;
+}
+
+const project = buildProject(userOptions.project.toString());
+
+// If the serve option is set, start the server on the given port
+if(project && options.serve) {
+  console.log(`Starting Server...`);
+
+  startServer(`${project.src}/dist`, {
+    port: userOptions.serve.toString()
+  }, project);
+}
